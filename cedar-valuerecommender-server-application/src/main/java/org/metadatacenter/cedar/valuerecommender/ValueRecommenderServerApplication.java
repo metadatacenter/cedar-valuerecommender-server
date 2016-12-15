c49577f41f8dca6496ba1a6aab83f80164099e5d
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-12-15 14:19:56 -0800

    Refactor

--- cedar-valuerecommender-server-application/src/main/java/org/metadatacenter/cedar/valuerecommender/ValueRecommenderServerApplication.java
+++ cedar-valuerecommender-server-application/src/main/java/org/metadatacenter/cedar/valuerecommender/ValueRecommenderServerApplication.java
@@ -3,25 +3,13 @@ package org.metadatacenter.cedar.valuerecommender;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
-import org.metadatacenter.bridge.CedarDataServices;
-import org.metadatacenter.cedar.valuerecommender.core.CedarAssertionExceptionMapper;
+import org.metadatacenter.cedar.util.dw.CedarDropwizardApplicationUtil;
 import org.metadatacenter.cedar.valuerecommender.health.ValueRecommenderServerHealthCheck;
 import org.metadatacenter.cedar.valuerecommender.resources.IndexResource;
 import org.metadatacenter.cedar.valuerecommender.resources.ValueRecommenderResource;
 import org.metadatacenter.config.CedarConfig;
 import org.metadatacenter.config.ElasticsearchConfig;
 import org.metadatacenter.intelligentauthoring.valuerecommender.ValueRecommenderService;
-import org.metadatacenter.server.security.Authorization;
-import org.metadatacenter.server.security.AuthorizationKeycloakAndApiKeyResolver;
-import org.metadatacenter.server.security.IAuthorizationResolver;
-import org.metadatacenter.server.security.KeycloakDeploymentProvider;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import java.util.EnumSet;
-
-import static org.eclipse.jetty.servlets.CrossOriginFilter.*;
 
 public class ValueRecommenderServerApplication extends Application<ValueRecommenderServerConfiguration> {
 
@@ -39,12 +27,8 @@ public class ValueRecommenderServerApplication extends Application<ValueRecommen
 
   @Override
   public void initialize(Bootstrap<ValueRecommenderServerConfiguration> bootstrap) {
-    // Init Keycloak
-    KeycloakDeploymentProvider.getInstance();
-    // Init Authorization Resolver
-    IAuthorizationResolver authResolver = new AuthorizationKeycloakAndApiKeyResolver();
-    Authorization.setAuthorizationResolver(authResolver);
-    Authorization.setUserService(CedarDataServices.getUserService());
+
+    CedarDropwizardApplicationUtil.setupKeycloak();
 
     cedarConfig = CedarConfig.getInstance();
 
@@ -70,19 +54,6 @@ public class ValueRecommenderServerApplication extends Application<ValueRecommen
     final ValueRecommenderServerHealthCheck healthCheck = new ValueRecommenderServerHealthCheck();
     environment.healthChecks().register("message", healthCheck);
 
-    environment.jersey().register(new CedarAssertionExceptionMapper());
-
-    // Enable CORS headers
-    final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-
-    // Configure CORS parameters
-    cors.setInitParameter(ALLOWED_ORIGINS_PARAM, "*");
-    cors.setInitParameter(ALLOWED_HEADERS_PARAM,
-        "X-Requested-With,Content-Type,Accept,Origin,Referer,User-Agent,Authorization");
-    cors.setInitParameter(ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD,PATCH");
-
-    // Add URL mapping
-    cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
-
+    CedarDropwizardApplicationUtil.setupEnvironment(environment);
   }
 }
