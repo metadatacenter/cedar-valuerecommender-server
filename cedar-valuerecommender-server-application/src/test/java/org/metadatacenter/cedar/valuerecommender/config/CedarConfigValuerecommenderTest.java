9f2b8636d8e3dc4d4217937acfd07398fd9dde5f
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-02 15:01:34 -0700

    Refactor to facilitate dockerization

--- cedar-valuerecommender-server-application/src/test/java/org/metadatacenter/cedar/valuerecommender/config/CedarConfigValuerecommenderTest.java
+++ cedar-valuerecommender-server-application/src/test/java/org/metadatacenter/cedar/valuerecommender/config/CedarConfigValuerecommenderTest.java
@@ -22,8 +22,12 @@ public class CedarConfigValuerecommenderTest {
 
     env.put(CedarEnvironmentVariable.CEDAR_KEYCLOAK_CLIENT_ID.getName(), "cedar-angular-app");
 
-    env.put(CedarEnvironmentVariable.CEDAR_MONGO_USER_NAME.getName(), "cedarUser");
-    env.put(CedarEnvironmentVariable.CEDAR_MONGO_USER_PASSWORD.getName(), "password");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_NAME.getName(), "cedarUser");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_PASSWORD.getName(), "password");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_PORT.getName(), "27017");
+
+    env.put(CedarEnvironmentVariable.CEDAR_PORT_VALUERECOMMENDER.getName(), "9006");
 
     env.put(CedarEnvironmentVariable.CEDAR_LD_USER_BASE.getName(), "https://metadatacenter.org/users/");
 
