9dc993b0442dce772f944ffe538a1acd3d865732
martinjoconnor <martin@zippyrate.com>
2026-09-08 00:32:47 -0700

    Document shared service routes in OpenAPI

---
+++ cedar-valuerecommender-server-application/src/test/java/org/metadatacenter/cedar/valuerecommender/resources/SharedOpenApiRoutesTest.java
@@ -0,0 +1,6 @@
+package org.metadatacenter.cedar.valuerecommender.resources;
+
+import org.metadatacenter.util.test.AbstractOpenApiSharedRoutesTest;
+
+class SharedOpenApiRoutesTest extends AbstractOpenApiSharedRoutesTest {
+}
